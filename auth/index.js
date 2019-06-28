9f53c4ae7313d098fa90517b47ec53f6f5f75811
Jason Prickett <prickettjw@gmail.com>
2019-06-27 22:50:45 -0700

    Refactored auth to use async await

--- auth/index.js
+++ auth/index.js
@@ -30,90 +30,75 @@ function generateToken() {
     return id;
 }
 
-router.post('/login', (req, res, next) => {
-    if(validUser(req.body)) {
-        User.getOneByEmail(req.body.email)
-        .then(user => {
+router.post('/login', async (req, res, next) => {
+    try{
+        if(validUser(req.body)) {
+            const user = await User.getOneByEmail(req.body.email);
             if(user) {
-                bcrypt.compare(req.body.password, user.password)
-                .then(result => {
-                    if(result){
-                        jwt.sign({user_id: user.id, role: user.role}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
-                            res.json({
-                                token,
-                                userId: user.id
-                            });
-                        });
-                    } else {
-                        res.status = 403
+                const result = await bcrypt.compare(req.body.password, user.password);
+                if(result){
+                    jwt.sign({user_id: user.id, role: user.role}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
                         res.json({
-                            status: 403,
-                            message: 'Email or password incorrect.'
-                        })
-                    }
-                });
-            } else {
-                res.status = 403
-                res.json({
-                    status: 403,
-                    message: 'Forbidden.'
-                })
-            };
-        });
-    };
-});
-
-router.post('/signup', (req, res, next) => {
-    console.log('Request!');
-    if(validUser(req.body)) {
-        User.getOneByEmail(req.body.email)
-        .then(user => {
-            if(!user) {
-                bcrypt.hash(req.body.password, 10)
-                .then(hash => {
-                    const user = {
-                        email: req.body.email,
-                        password: hash,
-                        display_name: req.body.displayName,
-                        created_at: new Date()
-                    };
-
-                    User.create(user)
-                    .then(user => {
-                        console.log(user);
-                        const key = generateToken();
-                        User.createVerification(user.id, key, user.email).then(returnData => {
-                            const mailOpts = {
-                                from: config.email.from,
-                                to: user.email,
-                                subject: 'Verify your IndexZer0 account.',
-                                text : `Please visit ${config.url}/verify/${returnData.key} to verify your account.`,
-                                html : `<p>Please visit <a href="${config.url}/verify/${returnData.key}">click here</a> to verify your IndexZer0 account.</p>`
-                            };
-
-                            mailer.sendMail(mailOpts, (err, response) => {
-                                if (err) {
-                                    console.log(err)
-                                } else {
-                                    console.log('Mail sent: ', response);
-                                }
-                            });
-                        }).then(() => {
-                            jwt.sign({user_id: user.id, role: user.role}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
-                                res.json({
-                                    token,
-                                    userId: user.id
-                                });
-                            });
+                            token,
+                            userId: user.id
                         });
                     });
-                });
+                } else {
+                    next(createError(403, 'Forbidden'));
+                }
             } else {
-                const err = new Error('A user with that email address already exists.');
-                err.status = 400;
-                next(err)
-            }
-        });
+                next(createError(403, 'Forbidden'));
+            };
+        };
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.post('/signup', async (req, res, next) => {
+    console.log('Request!');
+    if(validUser(req.body)) {
+        const user = await User.getOneByEmail(req.body.email)
+        if(!user) {
+            const hash = await bcrypt.hash(req.body.password, 10)
+            const user = {
+                email: req.body.email,
+                password: hash,
+                display_name: req.body.displayName,
+                created_at: new Date()
+            };
+
+            const newUser = await User.create(user);
+            const key = await generateToken();
+            const returnData = await User.createVerification(newUser.id, key, newUser.email)
+            const mailOpts = {
+                from: config.email.from,
+                to: newUser.email,
+                subject: 'Verify your IndexZer0 account.',
+                text : `Please visit ${config.url}/verify/${returnData.key} to verify your account.`,
+                html : `<p>Please visit <a href="${config.url}/verify/${returnData.key}">click here</a> to verify your IndexZer0 account.</p>`
+            };
+
+            await mailer.sendMail(mailOpts, (err, response) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log('Mail sent: ', response);
+                }
+            });
+
+            await jwt.sign({user_id: newUser.id, role: newUser.role}, process.env.JWT_SECRET, {expiresIn: '1d'}, (err, token) => {
+                res.json({
+                    token,
+                    userId: newUser.id
+                });
+            });
+
+        } else {
+            const err = new Error('A user with that email address already exists.');
+            err.status = 400;
+            next(err)
+        }
     } else {
         const err = new Error('Invalid email or password.');
         err.status = 400;
@@ -121,52 +106,39 @@ router.post('/signup', (req, res, next) => {
     }
 });
 
-router.put('/verify', (req, res, next) => {
+router.put('/verify', async (req, res, next) => {
     let key = req.body.key;
     if(key) {
-        User.getOneByVerificationKey(key)
-        .then(record => {
-            if(record) {
-                User.update(record.user_id, {active: true}).then(() => {
-                    User.deleteActivationRecord(record.user_id).then(() => {
-                        res.status(200).send();
-                    })
-                });
-            } else {
-                next(createError(403, 'Verification key is invalid.'));
-            }
-        })
+        const record = await User.getOneByVerificationKey(key)
+        if(record) {
+            await User.update(record.user_id, {active: true});
+            await User.deleteActivationRecord(record.user_id);
+
+            res.status(200).send();
+        } else {
+            next(createError(403, 'Verification key is invalid.'));
+        }
     }
 });
 
-router.put('/reset-password', verifyToken, (req, res, next) => {
+router.put('/reset-password', verifyToken, async (req, res, next) => {
     try {
         if(req.body.oldPassword) {
-            User.getOneById(req.userData.user_id)
-            .then(user => {
-                if(user) {
-                    // Verify user password and change it to new password'
-                    bcrypt.compare(req.body.oldPassword, user.password)
-                    .then(result => {
-                        if(result) {
-                            // Change pw to new pw
-                            bcrypt.hash(req.body.newPassword, 10)
-                            .then(hash => {
-                                console.log(hash);
-                                console.log(user.id);
-                                User.update(user.id, {password: hash})
-                                .then(() => {
-                                    res.status(200).json({message: 'Password updated.'});
-                                });
-                            });
-                        } else {
-                            createError(403, 'Password incorrect.');
-                        };
-                    });
+            const user = await User.getOneById(req.userData.user_id)
+            if(user) {
+                // Verify user password and change it to new password'
+                const result = await bcrypt.compare(req.body.oldPassword, user.password)
+                if(result) {
+                    // Change pw to new pw
+                    const hash = await bcrypt.hash(req.body.newPassword, 10);
+                    await User.update(user.id, {password: hash})
+                    res.status(200).json({message: 'Password updated.'});
                 } else {
-                    next(createError(403, 'Forbidden.'))
-                }
-            })
+                    next(createError(403, 'Password incorrect.'));
+                };
+            } else {
+                next(createError(403, 'Forbidden.'))
+            }
         } else {
             next(createError(401, 'User sent insufficient data.'));
         }
